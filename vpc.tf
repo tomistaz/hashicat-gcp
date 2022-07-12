6c4d57f465de5ff1943072c3b4135382da635031
Tom Istaz <istaz.tom@gmail.com>
2022-07-12 14:54:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,9 @@ module "network" {
   source  = "app.terraform.io/Tistaz-org/network/google"
   version = "3.4.0"
   # insert required variables here
+  network_name = "gaurav-network"
+  project_id = var.project  
+
   subnets = [
     {
       subnet_name   = "gaurav-subnet"
